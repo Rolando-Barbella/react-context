c812ffd6fdfae406a619ea0f29e7806cb51ba251
Rolando-Barbella <rolandobarbella@gmail.com>
2019-05-09 22:26:53 +0100

    Added context with hooks

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,36 +1,32 @@
-import React from 'react';
-import { UserConsumer } from './UserContext';
-import { EmailConsumer } from './context/EmailContext';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
+import { EmailContext } from './context/EmailContext';
 
-const MessageList = () => (
-  <UserConsumer>
-    {({ user, age }) => (
-      <EmailConsumer>
-        {({ loading, emails, onSelectEmail }) => (
-          <div className="MessageList">
-            {loading ? (
-              <div className="no-messages">Loading...</div>
-            ) : emails.length === 0 ? (
-              <div className="no-messages">
-                Your mailbox is empty, {user.firstName} - {age} years old! 🎉
-              </div>
-            ) : (
-              <ul>
-                {emails.map(email => (
-                  <Email
-                    key={email.id}
-                    email={email}
-                    onClick={() => onSelectEmail(email)}
-                  />
-                ))}
-              </ul>
-            )}
-          </div>
-        )}
-      </EmailConsumer>
-    )}
-  </UserConsumer>
-);
+const MessageList = () => {
+  const { user, age } = useContext(UserContext);
+  const { loading, emails, onSelectEmail } = useContext(EmailContext);
+  return (
+    <div className="MessageList">
+      {loading ? (
+        <div className="no-messages">Loading...</div>
+      ) : emails.length === 0 ? (
+        <div className="no-messages">
+          Your mailbox is empty, {user.firstName} - {age} years old! 🎉
+        </div>
+      ) : (
+        <ul>
+          {emails.map(email => (
+            <Email
+              key={email.id}
+              email={email}
+              onClick={() => onSelectEmail(email)}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+};
 
 const Email = ({ email, onClick }) => (
   <li onClick={onClick}>
